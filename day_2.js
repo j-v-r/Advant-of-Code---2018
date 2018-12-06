c7eb82f38e816b211b987de25360a8e1b962727b
j-v-r <john.vernon.reardon@gmail.com>
2018-12-06 06:26:46 -0600

    Advent of Code 2018 work
    My programs to solve Advent of Code 2018 problems -
    https://adventofcode.com/

---
+++ day_2.js
@@ -0,0 +1,162 @@
+/*'xdmgyjkpruszabaqwficevtjeo xdmgybkgwuszlbaqwfichvtneo xdmgyjkpruszlbcwwfichvtndo xdmgcjkprusyibaqwfichvtneo xdmgyjktruszlbwqwficuvtneo xdmgxjkpruszlbaqyfichvtnvo xdmgytkpruszlbaqwficuvtnlo xdmgydkpruszlbaqwfijhvtnjo xfmgyjkmruszlbaqwfichvtnes xdmgyrktruszlraqwfichvtneo xdmgyjkihuszlbaqdfichvtneo hdmgyjkpruszeiaqwfichvtneo xdmzyjkpruszlbaqwgichvtnxo xdmgyjknquszlbpqwfichvtneo idmgyjrpruszlbtqwfichvtneo xkmgyjkpruuzlbaqwfichvfneo xdmgyjkpruszlfaqwficnvtner xdmgyjkpruszlbpqwficwvteeo xdmgyjkpwuszlbiqwfhchvtneo xdmgyjkpruszwbaqwfichrtnbo xdpgyjkprusblbaqwfgchvtneo xdmryjkcruszlbaqwfichvtnee xwmgylkpruszlbaqwfcchvtneo xdmgyjkpruszflaqwfixhvtneo xdmgyjkmruszloaqwfichvteeo xvmgrjkpruszlbaqwfichvsneo xdmvyjkprusmlbaqwfichvtnes xdmgyjkpruszlbaqwfichkgbeo xdmgyikpruxzlbaqwfichvtnei xdmgyjkprugzlbaqhfichvtveo xdmgyjkpruszlbaqjaichftneo xdmzijkpruszlbaqwwichvtneo xdmgyjkprsszlbaqwfihhvlneo xdmgyjkprusqlwaqzfichvtneo ximgyjkpruszlbawwfichvtnen xsmgyjzpruszlbaqwfichvaneo xdmgyjkpruszlcaoyfichvtneo xdmgyjkprusmlbaqvnichvtneo xdmgyjkvruszmbaqwfichvtueo xdmgyjppuuszleaqwfichvtneo xddgyjkprubzlbaqwfichvaneo xdmgwjkpruszebaswfichvtneo xdogyjkpruszlblqwfichvdneo xdkgyjgpruszlbaqwfizhvtneo xdvgyjkpruszlbdqwfichvtqeo xdmgyjlpruszlbapwficgvtneo xdmgyjkpruszlbaqofickvtngo xdmgyjkprqszliaywfichvtneo xdqgyjkpruszlbcqwficnvtneo xdmgdjkpruszlbaqwxichvtseo xdmgyjkpruczlbaqwfichdtnfo xdmgyjkpruszluaqwficzvtnjo xdmgyjkproszlbaqwfacevtneo xfmgijkpruszlbrqwfichvtneo odmgyjkpluszlbaqwfichvuneo xdmgyjkpruszlbaqwwichukneo xdmgdjkpruszwbaqwfichvtnet xdmgyjkzrusvlbaqwrichvtneo xdmgylkprutzlbaqwfichvtnbo xdmgyjkpruszsbaqwfijtvtneo xdmgyjkproszlbjqwfichntneo xdmgyhkpluszlbaqwfichvtnlo xdmgyjhprushlbaqwfichvtnzo gdmoyjkpruszlbarwfichvtneo cdmgyjkpruszlbaqwfcchvtned xgmgyjkpruszlbaqwfschvtnek xdmgyjkprusnlzamwfichvtneo xdmgyjkprgszlbaxwfichvuneo txmgyjksruszlbaqwfichvtneo xdmgyjkprusbbbpqwfichvtneo xdmoyjkpruszlbaqwfighvtxeo xdmgyjkpruslhbaqwfichptneo xdmgzjkpruszlbaqwffcmvtneo xdmgyjkiruszlbaqgficuvtneo vdbgyjkpruszlbaqwfichvtnek xdmgyjspruszlbaqwfochvtney xdmgyjkpruszibaqwfivhvteeo xdmgyjkpruszfbaqwficbvtgeo xdmgyjkprystlbaqwxichvtneo xdmfyjkpryszlxaqwfichvtneo xdmgyjgpruspybaqwfichvtneo xdmgyjklruszlbjqwdichvtneo xdmgyjkzruszltaqwfichvtnek xdmgqjkpruszlzaqwfichvtneh xdmgyjhnruszlbaqwficqvtneo xdmgyjkproszlbaqweichvtnez xdmgyjkprurzlbaawfichytneo xdmgyfkpruszlbaqwfschutneo xdmnyjkpruszlbaawjichvtneo xdmgyjkpybszlbaqwfichvwneo xdmgtjkhruszlbaqwfichatneo xamgyjkprurzlbaqwfichvaneo xdmgyjkpruszlbaqwgichvtnqv ndmgyjkpruszlsaqwfuchvtneo xdmgygkpgusrlbaqwfichvtneo xdmgyjkpruszfbaqwfichvtnmy xdmgyjkprupflbaqwfichvjneo ndmgyjkpruszlbagwfichvtnxo xdmgyjkpruszlbafwfilhvcneo xdmgyjkpruszlbaqwfichvjsea xebgyjkpruszlbaqafichvtneo xdmkyjdpruszlbaqwfichvtnei xomgyjkprufzlbaqwfochvtneo xdmgyjkprfsllbaqwfiihvtneo xdmyyjkpruszebaqwficmvtneo xdmnyjkpruczlbarwfichvtneo xdmgyjkpruszcbaqwbichvtneg xdmgxjkpluszlbapwfichvtneo xgrlyjkpruszlbaqwfichvtneo xdmgyjkpruszlraqwxcchvtneo xdmhyjupruszlbaqafichvtneo xdmgnjkpruszlbkqwfjchvtneo xdmgyjkpruszlwaqwfichvtndg xdmgfjkpruvqlbaqwfichvtneo xdmgejkptuszlbdqwfichvtneo xlmgyjkpruszlnaqwfochvtneo xdmgcjkpruszlbaqwfiqhvaneo xdmgyjupruyzlbaywfichvtneo gdmgyjkpruyzlbaqwficevtneo xdmgyjkaruazlbapwfichvtneo xsmiyjkpruszlbaqwfichvtveo xdmiyjkprukzlbaqwfichvtnea xdbgmjkxruszlbaqwfichvtneo xdmgyjkpruskvbaqwfichdtneo xdmgyjkprusznbaqwficshtneo xdmgyjkprusrlbaqwfzchetneo xdmgyrkpruszzbaqwfichvtned xdmgyjkprusolbacwmichvtneo xdmgypkpruszlbaqwfichvtmgo xdmgyjkprumzlbhqwfichttneo xdmgydkprusglbaqwfichvtnei xdmuyjkpruszlbpqwfichvtyeo xdmtymkprusslbaqwfichvtneo xdmgyjjprkszlbaqwfqchvtneo xdmgvjdpruszlbaqwfichgtneo xdtgyjkpruwzlbaqwfjchvtneo xdmgyjkpruszlbafseichvtneo xdmgvjkpruszlraawfichvtneo xdmgyukprgszlbatwfichvtneo xhmgyjkpruszliaqwnichvtneo xdmgyjspruszlbwqyfichvtneo xdmgyjkjruszlqaqwfichvtnvo xdmgyjkiruszlbnqwfichmtneo ximgyjkpruszlbaqwfvcevtneo xdmdyjkpruszlbaqwsithvtneo ndmgyjkpruszlbaqwfilhatneo xdmgyjkpruszlbaqwfinhvcnez xdmgypkpsuszlbajwfichvtneo xdpgmjkpluszlbaqwfichvtneo xdmgyjnprupzlbaqwfichvtnel xbmgyjkprmszlfaqwfichvtneo xdmgyjkpausllbaqwfichvtseo xdmgyjkpruszlbaqwfqchttnes xgmgyjkpruszlbaxwfichvtneb xdmgyjkpruszabqqwfichvineo xdmgpjkpquszlbaqwfichvdneo xdmgyjkeruszlbaqdficbvtneo xdmaujkpruszlbaqwfichvteeo xdmgyjkpruszlbaqwrirhvtnev xdmgyjkpsugzllaqwfichvtneo xdmgyjkpruszlbaqwfichctnlm xdmeyjkpruszlbacwfiwhvtneo xdmgyjkpiuhzlbaqwfijhvtneo xdmgyjkpruszlbmqhfiohvtneo xdegyjkpbuszlbbqwfichvtneo xdmggxkpruszlbaqwfirhvtneo xdmgojkpruszlbaqvfichvtteo xdmgyjhtruszlbaqwmichvtneo rdmgyjkpruszlbaqwfichvthek xdlgyjqpruszlbaqwfbchvtneo xdmgyjspriszlbavwfichvtneo rdkgyjkpruszlbaqwfichvtnuo tdmgyjkuruszlbaqwfichvtnev xdmgyjkpxuszlbaqwfkchvtnso xdegyjkpruszlbbqxfichvtneo xdmgyjkpruszlbaqwficpvtket xdmgyjkpruszliaqwfnchvtnec xdmgyjkpreszlbaqwficdvtdeo rdmgyjkpruszlbaywfychvtneo xdmgywkpruszlbaqwficrvtaeo xdmgyjkpruszlbanwflchvoneo xdmgyjkpruyzlbaqufychvtneo symgyjkpruszlbaqwfichvtqeo xdmgyjkpruszlbaqwfichvbzqo xzfgyjkpruszlbaqwfichvtveo udmgyjepruszlbaqwfichbtneo xhmgyjkpruszlbaqwfjchvtnef xdhgyjkpruszlbaqaftchvtneo xdmzyjkjruszlbaqwfichvtnwo xdmgyjepruszlbaqwffchvtnef xdmgyjkprurzlbaqwfikhvtneq xomoyjkpruszkbaqwfichvtneo xdmgyjkpiuszubaqwfichktneo xdmgyjkprusdlbaqwhihhvtneo xdmgyjkpruszlbaqwwirhvxneo xdmgyjkpruszlbaqwficgitzeo xdmgyjlpruszlbaqwfichpjneo xjmgyjkpxuszlbaqwfichatneo xdmgylkpruszlbaqwfiehvtnez xdmgbjkpruszmbaqwfihhvtneo xdmgyjkprubzlwaqwfichvtxeo xdmgyjhlrustlbaqwfichvtneo xdmmyjkpruszlbaqwfdchitneo xdmgyjkpruszlbaqwoichhtbeo xdzgyjkprvszlcaqwfichvtneo ndmgyjkpruszlbaqwficavxneo xdmgyjfpruszxbaqzfichvtneo xdmgyjkpeuszlbaqzficdvtneo xdmgyjkpruszlbmqffidhvtneo xdnvyjkpruszlbafwfichvtneo xdygyjkpruszlbljwfichvtneo xdigyjkpruszlbeqwfuchvtneo xdmgyjkpruszlbpzwfichvteeo bdmgyjbpruszldaqwfichvtneo xdmgyjkprrszlbaqmpichvtneo idmgyjkpruszlbaqyfichvtkeo xdmgyjkmrqsclbaqwfichvtneo xdmgyjkpruazlbeqwfichvtxeo ddmgyjkpruszlbsqwfichotneo xdmgyqkpruszjbaqwfxchvtneo xdmnyjkpruozlbaqwfichvtreo edmgyjkpruszlbuqwwichvtneo xdmgyjkprmshlbaqwfichctneo xdmgyjkpruszlbaqwffghotneo xdmcyjkprfszlbaqnfichvtneo xdmgyjypruszhbaqwficyvtneo xdmgyjkprzszlyaqwficmvtneo xlmgyjkprzszlbaqwficyvtneo xdmgyjkprutulbaqwfithvtneo xdygyjkpruszlbpqwfichvpneo xdmgsjkpoumzlbaqwfichvtneo xdmgyjkpyuszlbaqdfnchvtneo xdxgyjkpruszlbaqwfizhvtnjo xdmgyjkpruszlbaqwfschvkndo xdmgpjkprnszlcaqwfichvtneo xhmgyjkpruszlbaqwficgvtnet xdmgyjkpruswlbaqwfichvtqer ddmgyjkprcszlbaqwfichqtneo xdmgyjkpruhhlbaqwpichvtneo xdmgyjkeraszlbaqwfichvtnso nomgyjkpruszlbaqwficavxneo xdmgyjkprdszlbaqwfobhvtneo xdmgyjkprgszlbaqwfichvtdao xomgyjspruswlbaqwfichvtneo xdzgyjkpruszlbaqwficwvpneo admgejkpruszlbaqwfimhvtneo xdtgyjkpruszlmaqwfiqhvtneo xdmgymkprusqlbaqwtichvtneo xdmgyjkpluszlbaqwfidhvtnea ztmgyjjpruszlbaqwfichvtneo'*/
+function exe() {
+	const arrayOfBoxIds = 'abcde fghij klmno pqrst fguij axcye wvxyz'.split(' ');
+
+	let thisWord = 0;
+	let comparedWord = 1;
+
+	while(thisWord < arrayOfBoxIds.length) {
+	
+		for(let i = comparedWord; i < arrayOfBoxIds.length; i++) {
+
+			for(let j = 0; j < arrayOfBoxIds[thisWord].length; j++) {
+				let numOfDiffChars = 0;
+				let sameChars = '';
+				let thisChar = arrayOfBoxIds[thisWord][j];
+				let comparedChar = arrayOfBoxIds[comparedWord][j];
+				if(thisChar != comparedChar) {
+					numOfDiffChars++;
+				}
+				else {
+					sameChars += thisChar;
+				}
+				if(numOfDiffChars === 1) {
+					return sameChars;
+				}
+			}		
+		}
+	comparedWord++;	
+	thisWord++;	
+	}
+}
+
+console.log(exe());
+
+	// for(let word of arrayOfBoxIds) {
+	// 	let charMap = {};
+		
+	// 	for(let letter of word) {
+	// 		if(!charMap[letter]) {
+	// 			charMap[letter] = 1;
+	// 		}	
+	// 		else {
+	// 			charMap[letter]++
+	// 		}
+	// 	}
+	// 	arrayOfMaps.push(charMap);
+	// }
+
+	// for(let map in arrayOfMaps) {
+	// 	console.log(arrayOfMaps[map]);
+
+
+	// }
+
+	// let arrayOfMaps = [];
+
+	// for(let word of arrayOfBoxIds) {
+	// 	let charMap = {};
+		
+	// 	for(let letter of word) {
+			
+	// 		if(!charMap[letter]) {
+	// 			charMap[letter] = 1;
+	// 		}	
+	// 		else {
+	// 			charMap[letter]++
+	// 		}
+	// 	}
+		
+	// 	arrayOfMaps.push(charMap);
+	// }
+
+	// for(let map of arrayOfMaps) {
+		
+
+	// }
+
+	// for(let word of arrayOfBoxIds) {
+	// 	console.log(word);
+	// }
+
+	
+
+
+
+
+	// let wordsWithTwoOfAnyLetter = [];
+	// let wordsWithThreeOfAnyLetter = [];
+
+
+	// for(let i = 0; i < arrayOfBoxIds.length; i++) {
+	// let charMap = {};
+	// 	for(let j = 0; j < arrayOfBoxIds[i].length; j++) {
+	// 		if(!charMap[arrayOfBoxIds[i][j]]) {
+	// 			charMap[arrayOfBoxIds[i][j]] = 1;
+	// 		}
+	// 		else {
+	// 			charMap[arrayOfBoxIds[i][j]]++;
+	// 		}
+	// 	}
+	// 	for(char in charMap) {
+	// 		let charNum = charMap[char];
+	// 		if(charNum === 2) {
+	// 			if(!wordsWithTwoOfAnyLetter.includes(arrayOfBoxIds[i])) {
+	// 				wordsWithTwoOfAnyLetter.push(arrayOfBoxIds[i]);
+	// 			}
+	// 		}
+	// 		if(charNum === 3) {
+	// 			if(!wordsWithThreeOfAnyLetter[arrayOfBoxIds[i]]) {
+	// 				wordsWithThreeOfAnyLetter.push(arrayOfBoxIds[i]);
+	// 			}
+	// 		}
+	// 	}
+	// }
+
+	// return wordsWithThreeOfAnyLetter.length * wordsWithTwoOfAnyLetter.length;
+
+
+
+
+
+/*
+[create array of box ids from string input]
+[create charMap object to hold number of times each char appears in each word]
+[create variable to hold total num of words that have a letter appear exactly twice]
+[create variable to hold total sum of words that have a letter appear exactly three times]
+[Loop over each string in array of ids]
+
+let pointer1 = 0;
+	let index1 = 1;
+
+	while(index1 < arrayOfBoxIds.length) {
+
+		for(let i = index1; i < arrayOfBoxIds.length; i++) {
+			let thisWord = arrayOfBoxIds[pointer1];
+			let comparedWord = arrayOfBoxIds[index1];
+			let pointer2 = 0;
+
+			while(pointer2 < comparedWord.length) {
+				
+				for(let j = 0; j < comparedWord.length; j++) {
+	
+					if(thisWord[pointer2] === comparedWord[j]) {
+						console.log(thisWord[pointer2])
+					}
+				}
+
+				pointer2++;
+			}
+		}
+
+		pointer1++;
+		index1++;
+	}
+*/
+
+
+
+
+
+
+
